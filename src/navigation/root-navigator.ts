796ad5cdb27e74a02900f3dc90bf6a77bf97235e
Ebou <ebou.jobe@mail.mcgill.ca>
2018-07-30 18:09:26 -0400

    linear-screen initial implementation

--- src/navigation/root-navigator.ts
+++ src/navigation/root-navigator.ts
@@ -1,8 +1,10 @@
 import { createStackNavigator } from "react-navigation"
+import { LinearEquation } from "../views/example/linear-equation/linear-equation-screen"
 import { ExampleNavigator } from "./example-navigator"
 
 export const RootNavigator = createStackNavigator(
   {
+    linearEquation: { screen: LinearEquation },
     exampleStack: { screen: ExampleNavigator },
   },
   {
---
+++ src/views/example/linear-equation/index.ts
@@ -0,0 +1,1 @@
+export * from "./linear-equation-screen"
