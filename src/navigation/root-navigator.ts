6f60f32f39416d10182168f614edc5eeff626fd7
Ebou <ebou.jobe@mail.mcgill.ca>
2018-07-30 18:11:00 -0400

    cubic-screen initial implementation

--- src/navigation/root-navigator.ts
+++ src/navigation/root-navigator.ts
@@ -1,10 +1,12 @@
 import { createStackNavigator } from "react-navigation"
+import { CubicEquation } from "../views/example/cubic-equation/cubic-equation-screen"
 import { QuadraticEquation } from "../views/example/quadratic-equation/quadratic-equation-screen"
 import { LinearEquation } from "../views/example/linear-equation/linear-equation-screen"
 import { ExampleNavigator } from "./example-navigator"
 
 export const RootNavigator = createStackNavigator(
   {
+    cubicEquation: { screen: CubicEquation },
     quadraticEquation: { screen: QuadraticEquation },
     linearEquation: { screen: LinearEquation },
     exampleStack: { screen: ExampleNavigator },
